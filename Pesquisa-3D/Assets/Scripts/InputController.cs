a648c3b7a8e68b02a909b24140a092105402a7f9
Jhone Mendonça <g.m.enohj@gmail.com>
2016-04-08 16:58:47 -0400

    Personagem e alguns movimentos

---
+++ Pesquisa-3D/Assets/Scripts/InputController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+    private MovementController movementController;
+    string input;
+
+
+	// Use this for initialization
+	void Start () {
+        
+
+        movementController = GetComponent<MovementController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            //Debug.Log("left");
+            input = "LEFT";
+            movementController.sendInput(input);
+        }
+        else if(Input.GetAxis("Horizontal") > 0)
+        {
+            //Debug.Log("right");
+            input = "RIGHT";
+            movementController.sendInput(input);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            //Debug.Log("back");
+            input = "BACK";
+            movementController.sendInput(input);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            //Debug.Log("front");
+            input = "FRONT";
+            movementController.sendInput(input);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Debug.Log("jump");
+            input = "JUMP";
+            movementController.sendInput(input);
+        }
+    }
+}
---
+++ Pesquisa-3D/Assets/Scripts/MovementController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject ObjectToMove;
+    [SerializeField]
+    private float StepDistance = 0.1f;
+
+
+    private bool isJumping = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(ObjectToMove.GetComponent<Rigidbody>().velocity.y == 0)
+        {
+            isJumping = false;
+        }
+	}
+
+    public void sendInput(string input)
+    {
+        switch(input)
+        {
+            case "JUMP":
+                jumpObject();
+                break;
+            case "FRONT":
+                moveObjectToFront();
+                break;
+            case "BACK":
+                moveObjectToBack();
+                break;
+            case "LEFT":
+                jumpObject();
+                break;
+            case "RIGHT":
+                jumpObject();
+                break;
+        }
+    }
+
+    private void jumpObject()
+    {
+        if(!isJumping)
+        {
+            isJumping = true;
+            ObjectToMove.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1000));
+        }
+    }
+
+    private void moveObjectToBack()
+    {
+        //Debug.Log("front");
+
+        float zMovement;
+        float xMovement;
+
+        zMovement = Mathf.Cos(ObjectToMove.transform.rotation.y * Mathf.Deg2Rad) * StepDistance;
+        xMovement = Mathf.Sin(ObjectToMove.transform.rotation.y * Mathf.Deg2Rad) * StepDistance;
+
+        Vector3 newPosition =
+            new Vector3(ObjectToMove.transform.position.x - xMovement,
+            ObjectToMove.transform.position.y,
+            ObjectToMove.transform.position.z - zMovement);
+
+        ObjectToMove.transform.position = newPosition;
+    }
+
+    private void moveObjectToFront()
+    {
+        //Debug.Log("front");
+        
+        float zMovement;
+        float xMovement;
+
+        zMovement = Mathf.Cos(ObjectToMove.transform.rotation.y * Mathf.Deg2Rad) * StepDistance;
+        xMovement = Mathf.Sin(ObjectToMove.transform.rotation.y * Mathf.Deg2Rad) * StepDistance;
+
+        Vector3 newPosition = 
+            new Vector3(ObjectToMove.transform.position.x + xMovement,
+            ObjectToMove.transform.position.y,
+            ObjectToMove.transform.position.z + zMovement);
+
+        ObjectToMove.transform.position = newPosition;
+    }
+}
