29cd9f23fc41ef8a306120a4544c839e74632fad
Jhone Mendonça <g.m.enohj@gmail.com>
2016-05-05 11:30:29 -0400

    Camera

---
+++ Pesquisa-3D/Assets/LookAt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+
+    public Transform LookAtTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(
+            new Vector3(
+                LookAtTarget.position.x,
+                0,
+                LookAtTarget.position.z));
+	}
+}
